a5ce17c4ca5b9e413de3df7a4447e720525327f2
akii <akiichris01@gmail.com>
2020-10-26 08:48:20 -0400

    commit

---
+++ TaskList/TaskItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TaskList
+{
+    internal class TaskItem
+    {
+        public TaskItem(string v1, string v2, DateTime dateTime)
+        {
+        }
+
+        public object Description { get; internal set; }
+        public object DueDate { get; internal set; }
+        public object TaskOwner { get; internal set; }
+        public bool Complete { get; internal set; }
+    }
+}
